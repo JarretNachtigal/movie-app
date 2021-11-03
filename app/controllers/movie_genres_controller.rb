f9d63ccf755cff82cb26987e336141b60834e73f
Jarret Nachtigal <jarretn1224@gmail.com>
2021-11-02 22:19:07 -0400

    added create to movie_genres controller

--- app/controllers/movie_genres_controller.rb
+++ app/controllers/movie_genres_controller.rb
@@ -1,5 +1,13 @@
 class MovieGenresController < ApplicationController
   def create
-    
+    mgenre = MovieGenre.new(
+      movie_id: params[:movie_id],
+      genre_id: params[:genre_id]
+    )
+    if mgenre.save
+      render json: mgenre.as_json
+    else
+      render json: { mgenre.errors.full_messages  }, status: :unprocessable_entity
+    end
   end
 end
